067b45dc9249397159d585e43764baebd151b600
aeed mami <s.emami@mobinnet.ir>
2025-11-19 11:10:00 +0330

    Change the header token to an HttpOnly cookie in the authMiddleware.js file

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,20 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 function authMiddleware(req, res, next) {
-  const authHeader = req.headers.authorization;
+  const token = req.cookies.token; // از کوکی بگیر
 
-  if (!authHeader) {
+  if (!token) {
     return res.status(401).json({ message: "No token provided" });
   }
 
-  // انتظار داریم توکن به شکل "Bearer <token>" باشه
-  const token = authHeader.split(" ")[1];
-
   try {
-    // بررسی اعتبار و تاریخ انقضای توکن
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // اطلاعات کاربر رو ذخیره می‌کنیم تا در ادامه قابل دسترسی باشه
-    next(); // برو به مرحله بعد
+    req.user = decoded; // اطلاعات کاربر
+    next();
   } catch (err) {
     if (err.name === "TokenExpiredError") {
       return res.status(401).json({ message: "Token expired" });
