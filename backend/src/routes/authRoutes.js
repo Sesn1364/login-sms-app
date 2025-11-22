e8fd026f99bb24d445b62997613ecb09a5a2c728
aeed mami <s.emami@mobinnet.ir>
2025-11-22 10:05:09 +0330

    Connecting the middleware to the user login route in the authRoutes.js file

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { registerUser, loginUser, getMe } = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const verifyCaptcha = require('../middlewares/verifyCaptcha');
 
 const router = express.Router();
 
 router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/login', verifyCaptcha, loginUser);
 
 //مسیر دریافت اطلاعات کامل کاربر
 router.get('/me', authMiddleware, getMe);
